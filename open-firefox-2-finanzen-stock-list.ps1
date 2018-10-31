9ed46fb2585f14d228f7893997cbe0d427ecd71c
dan-stock <34940321+dan-stock@users.noreply.github.com>
2018-10-31 21:02:12 +0100

    powershell script that opens pages in firefox

---
+++ open-firefox-2-finanzen-stock-list.ps1
@@ -0,0 +1,46 @@
+
+$stocks = @('US00507V1098', 'LU1681045537', 'US01609W1027', 'US02079K1079')
+
+$stocks = @('US00507V1098', 'LU1681045537', 'US01609W1027', 'US02079K1079', 'US02079K3059', 'US0231351067', 'US0378331005', 'GB00BZ09BD16', 'US2546871060', 'LU0029871042', 'US30303M1027', 'DE0005785802', 'DE0006048432', 'US46120E6023', 'DE000A0F5UF5', 'US58733R1023', 'US5949181045', 'US6541061031', 'US67066G1040', 'US70450Y1038', 'CA82509L1076', 'US8486371045', 'KYG875721634', 'GB0002771383', 'DE0007472060')
+
+
+foreach ($stock in $stocks) {[system.Diagnostics.Process]::Start("firefox","https://www.finanzen.net/aktien/$stock")}
+
+
+## -Analysen
+
+US00507V1098
+US01609W1027
+US02079K1079
+LU1681045537
+US02079K3059
+LU0029871042
+US30303M1027
+
+
+https://www.finanzen.net/aktien/Activision_Blizzard-Aktie
+https://www.finanzen.net/aktien/Alibaba-Aktie
+https://www.finanzen.net/aktien/Alphabet-Aktie
+https://www.finanzen.net/aktien/Wirecard-Aktie
+https://www.finanzen.net/aktien/Tencent-Aktie
+https://www.finanzen.net/aktien/PayPal-Aktie
+https://www.finanzen.net/aktien/Shopif-Aktie
+https://www.finanzen.net/aktien/NVIDIA-Aktie
+https://www.finanzen.net/aktien/Nike-Aktie
+https://www.finanzen.net/aktien/MercadoLibre-Aktie
+https://www.finanzen.net/aktien/Henkel_vz-Aktie
+https://www.finanzen.net/aktien/Intuitive_Surgical-Aktie
+https://www.finanzen.net/aktien/Microsoft-Aktie
+https://www.finanzen.net/aktien/Fresenius_Medical_Care-Aktie
+https://www.finanzen.net/aktien/Atlassian-Aktie
+https://www.finanzen.net/aktien/Walt_Disney-Aktie
+https://www.finanzen.net/aktien/Amazon-Aktie
+https://www.finanzen.net/aktien/Apple-Aktie
+https://www.finanzen.net/aktien/Splunk-Aktie
+https://www.finanzen.net/aktien/facebook-Aktie
+
+
+#https://www.finanzen.net/etf/amundi_is_sicav_-_msci_world_ex_europe_ucits_etf_eur
+https://www.finanzen.net/etf/ishares_nasdaq-100_r_ucits_etf_de
+#https://www.finanzen.net/fonds/threadneedle_european_smaller_comp
+https://www.finanzen.net/fonds/templeton_global_bond_fund_a_mdis_usd
