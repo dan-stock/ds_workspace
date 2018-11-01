66a591be78f3438e6fc20e0390cc3756b0b8600d
dan-stock <34940321+dan-stock@users.noreply.github.com>
2018-11-01 21:50:48 +0100

    change list

--- open-firefox-2-finanzen-stock-list.ps1
+++ open-firefox-2-finanzen-stock-list.ps1
@@ -3,6 +3,9 @@ $stocks = @('US00507V1098', 'LU1681045537', 'US01609W1027', 'US02079K1079')
 
 foreach ($stock in $stocks) {[system.Diagnostics.Process]::Start("firefox","https://www.finanzen.net/aktien/$stock")}
 
+$stocks = @('Activision_Blizzard','Alibaba','Alphabet','Wirecard','Tencent','PayPal','Shopif','NVIDIA','Nike','Henkel_vz')
+$stocks = @('Intuitive_Surgical','Microsoft','Fresenius_Medical_Care','Atlassian','Walt_Disney','Amazon','Apple','Splunk','facebook')
+
 
 
 https://www.finanzen.net/aktien/Activision_Blizzard-Aktie
